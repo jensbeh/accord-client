e2f51177ed083f050fb993fd03ad3d007c914304
Rasched <rasched.f@gmail.com>
2021-05-06 12:33:58 +0200

    fix: add test method for homeview

--- src/test/java/de/uniks/stp/SettingsControllerTest.java
+++ src/test/java/de/uniks/stp/SettingsControllerTest.java
@@ -1,8 +1,6 @@
 package de.uniks.stp;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,7 +20,36 @@ public class SettingsControllerTest extends ApplicationTest {
     }
 
     @Test
-    public void changeLanguage() {
+    public void changeLanguageLogin() {
+        Button settingsButton = lookup("#settingsButton").query();
+        clickOn(settingsButton);
+        Button languageButton = lookup("#button_Language").query();
+        clickOn(languageButton);
+        Label label_langSelect = lookup("#label_langSelect").query();
+        ComboBox<String> comboBox_langSelect = lookup("#comboBox_langSelect").query();
+
+        clickOn(comboBox_langSelect);
+        clickOn("Deutsch");
+        Assert.assertEquals("Sprache", languageButton.getText());
+        Assert.assertEquals("Sprache ausw\u00e4hlen:", label_langSelect.getText());
+
+        clickOn(comboBox_langSelect);
+        clickOn("English");
+        Assert.assertEquals("Language", languageButton.getText());
+        Assert.assertEquals("Select Language:", label_langSelect.getText());
+    }
+
+    @Test
+    public void changeLanguageHomeScreen() throws InterruptedException {
+        TextField usernameTextField = lookup("#usernameTextfield").query();
+        usernameTextField.setText("peter");
+        PasswordField passwordField = lookup("#passwordTextField").query();
+        passwordField.setText("123");
+        CheckBox rememberBox = lookup("#rememberMeCheckbox").query();
+        rememberBox.setSelected(true);
+        clickOn("#loginButton");
+        Thread.sleep(500);
+
         Button settingsButton = lookup("#settingsButton").query();
         clickOn(settingsButton);
         Button languageButton = lookup("#button_Language").query();
