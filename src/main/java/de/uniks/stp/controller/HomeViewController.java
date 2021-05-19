315d2563ff953cfcb0e525aac43fb2c50416930a
Alex <alexbenz7@web.de>
2021-05-19 19:42:54 +0200

    fix: Change the size of createServerView
    Now always closes the Stage since it sometimes didn't close properly. Also change the size of the createServerView to show the complete No connection message without needing to resize the view

--- src/main/java/de/uniks/stp/controller/HomeViewController.java
+++ src/main/java/de/uniks/stp/controller/HomeViewController.java
@@ -235,6 +235,8 @@ public class HomeViewController {
         this.homeCircle.setOnMouseClicked(null);
         this.settingsButton.setOnAction(null);
         this.logoutButton.setOnAction(null);
+        this.stage.close();
+        stage = null;
         try {
             if (builder.getSERVER_USER() != null) {
                 if (builder.getSERVER_USER().getSession() != null) {
