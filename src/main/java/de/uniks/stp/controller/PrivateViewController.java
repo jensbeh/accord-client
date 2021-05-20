acab9374969e95eb319eb9fc639d2e1b8284d7ce
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2021-05-20 14:42:29 +0200

    repaired broken Merge and corrected error in noConnectionWebS test

--- src/main/java/de/uniks/stp/controller/PrivateViewController.java
+++ src/main/java/de/uniks/stp/controller/PrivateViewController.java
@@ -46,9 +46,7 @@ public class PrivateViewController {
     private ModelBuilder builder;
     private VBox userBox;
     private VBox currentUserBox;
-    private VBox messages;
     private VBox chatBox;
-    private HBox messageBar;
     private HBox viewBox;
     private ObservableList<Channel> privateChats;
     private ListView<Channel> privateChatList;
@@ -118,47 +116,39 @@ public class PrivateViewController {
                         }
                     }
                     if (jsonObject.containsKey("action") && jsonObject.getString("action").equals("info")) {
+                        String errorTitle;
                         String serverMessage = jsonObject.getJsonObject("data").getString("message");
-                        if (!serverMessage.equals("This is not your username.")) {
-                            Platform.runLater(() -> {
-                                Alert alert = new Alert(Alert.AlertType.INFORMATION, "", ButtonType.OK);
-                                alert.setTitle("Chat Error");
-                                alert.setHeaderText(serverMessage);
-                                Optional<ButtonType> result = alert.showAndWait();
-                            });
+                        if (serverMessage.equals("This is not your username.")) {
+                            errorTitle = "Username Error";
+                        } else {
+                            errorTitle = "Chat Error";
                         }
-                    }
-                }
-                if (jsonObject.containsKey("action") && jsonObject.getString("action").equals("info")) {
-                    String errorTitle;
-                    String serverMessage = jsonObject.getJsonObject("data").getString("message");
-                    if (serverMessage.equals("This is not your username.")) {
-                        errorTitle = "Username Error";
-                    } else{
-                        errorTitle = "Chat Error";
-                    }
                         Platform.runLater(() -> {
                             Alert alert = new Alert(Alert.AlertType.INFORMATION, "", ButtonType.OK);
                             alert.setTitle(errorTitle);
                             alert.setHeaderText(serverMessage);
                             Optional<ButtonType> result = alert.showAndWait();
-                        });
-                }
-            }
-
-            @Override
-            public void onClose(Session session, CloseReason closeReason) {
-                System.out.println(closeReason.getCloseCode().toString());
-                if (!closeReason.getCloseCode().toString().equals("NORMAL_CLOSURE")) {
-                    Platform.runLater(() -> {
-                        Alert alert = new Alert(Alert.AlertType.ERROR, "No connection to server.", ButtonType.OK);
-                        alert.setTitle("Error Dialog");
-                        alert.setHeaderText("No Connection");
-                        Optional<ButtonType> result = alert.showAndWait();
                             if (result.isPresent() && result.get() == ButtonType.OK) {
                                 showUsers();
                             }
-                    });
+                        });
+                    }
+                }
+
+                @Override
+                public void onClose(Session session, CloseReason closeReason) {
+                    System.out.println(closeReason.getCloseCode().toString());
+                    if (!closeReason.getCloseCode().toString().equals("NORMAL_CLOSURE")) {
+                        Platform.runLater(() -> {
+                            Alert alert = new Alert(Alert.AlertType.ERROR, "", ButtonType.OK);
+                            alert.setTitle("No Connection Error");
+                            alert.setHeaderText("No Connection - Please check and try again later");
+                            Optional<ButtonType> result = alert.showAndWait();
+                            if (result.isPresent() && result.get() == ButtonType.OK) {
+                                showUsers();
+                            }
+                        });
+                    }
                 }
             });
             builder.setPrivateChatWebSocketCLient(privateChatWebSocketCLient);
