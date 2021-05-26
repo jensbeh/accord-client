134dc11b2a973f089e96a644cf29388819ea16b0
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2021-05-26 22:34:49 +0200

    added Test for change in CategorySubVIew

--- src/test/java/de/uniks/stp/ServerViewControllerTest.java
+++ src/test/java/de/uniks/stp/ServerViewControllerTest.java
@@ -1,5 +1,6 @@
 package de.uniks.stp;
 
+import de.uniks.stp.model.Channel;
 import de.uniks.stp.model.Server;
 import de.uniks.stp.model.User;
 import de.uniks.stp.net.RestClient;
@@ -34,7 +35,7 @@ public class ServerViewControllerTest extends ApplicationTest {
     @BeforeClass
     public static void setupHeadlessMode() {
         System.setProperty("testfx.robot", "glass");
-        System.setProperty("testfx.headless", "true");
+        System.setProperty("testfx.headless", "false");
     }
 
     @Override
@@ -173,5 +174,23 @@ public class ServerViewControllerTest extends ApplicationTest {
 
         restClient.logout(testUserOne_UserKey, response -> {
         });
+    }
+
+
+    @Test
+    public void categoryViewTest() throws InterruptedException {
+        getServerId();
+        loginInit(testUserOneName, testUserOnePw);
+
+        ListView<Server> serverList = lookup("#scrollPaneServerBox").lookup("#serverList").query();
+        clickOn(serverList.lookup("#serverName_" + testServerId));
+        Thread.sleep(2000);
+
+        ListView channels = lookup("#channellist").queryListView();
+        app.getBuilder().getCurrentServer().getCategories().get(0).withChannel(new Channel().setName("PARTEY"));
+        Assert.assertEquals(app.getBuilder().getCurrentServer().getCategories().get(0).getChannel().size(),channels.getItems().size());
+
+        clickOn("#logoutButton");
+        Thread.sleep(2000);
     }
 }
