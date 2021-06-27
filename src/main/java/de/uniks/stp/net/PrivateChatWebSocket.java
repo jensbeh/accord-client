4c8157458cd64bf42447af37331a549cdb2fb506
Alex <alexbenz7@web.de>
2021-06-27 20:17:46 +0200

    fix: moved the alert.onClose above to work

--- src/main/java/de/uniks/stp/net/PrivateChatWebSocket.java
+++ src/main/java/de/uniks/stp/net/PrivateChatWebSocket.java
@@ -99,8 +99,8 @@ public class PrivateChatWebSocket extends Endpoint {
                 Alert alert = new Alert(Alert.AlertType.ERROR, "", ButtonType.OK);
                 alert.setTitle(StageManager.getLangBundle().getString("error.no_connection"));
                 alert.setHeaderText(StageManager.getLangBundle().getString("error.no_connection_text"));
+                alert.setOnCloseRequest(e -> StageManager.showLoginScreen());
                 Optional<ButtonType> result = alert.showAndWait();
-                alert.setOnCloseRequest(e -> {StageManager.showLoginScreen();});
                 if (result.isPresent() && result.get() == ButtonType.OK) {
                     StageManager.showLoginScreen();
                 }
