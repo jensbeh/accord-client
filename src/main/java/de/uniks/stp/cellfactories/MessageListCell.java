45d2660cd05eb29de75259613be2922851ed4269
Maurice Golob <maurice.golob@gmail.com>
2021-07-05 18:07:06 +0200

    fix: merged new master

--- src/main/java/de/uniks/stp/cellfactories/MessageListCell.java
+++ src/main/java/de/uniks/stp/cellfactories/MessageListCell.java
@@ -13,6 +13,9 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -66,6 +69,7 @@ public class MessageListCell implements javafx.util.Callback<ListView<Message>, 
 
     private static class MessageCell extends ListCell<Message> {
         private boolean loadImage;
+        private boolean loadVideo;
         private String urlType;
 
         /**
@@ -94,12 +98,18 @@ public class MessageListCell implements javafx.util.Callback<ListView<Message>, 
                 String textMessage = item.getMessage();
                 String url = searchUrl(textMessage);
                 loadImage = false;
+                loadVideo = false;
                 WebView webView = new WebView();
+                MediaView mediaView = new MediaView();
                 if (!url.equals("") && !url.contains("https://ac.uniks.de/")) {
-                    setImage(url, webView.getEngine());
+                    setMedia(url, webView.getEngine(), mediaView);
                     if (loadImage) {
                         webView.setContextMenuEnabled(false);
                         setImageSize(url, webView);
+                        textMessage = textMessage.replace(url, "");
+                    }
+                    if (loadVideo) {
+                        setVideoSize(url, mediaView);
                         textMessage = textMessage.replace(url, "");
                     }
                 }
@@ -129,15 +139,16 @@ public class MessageListCell implements javafx.util.Callback<ListView<Message>, 
                     message.parseAndAppend(" " + str + " ");
                 }
 
-                if (!loadImage) {
-                    vbox.getChildren().addAll(userName, message);
-                } else {
+                if (loadImage){
                     vbox.getChildren().addAll(userName, message, webView);
+                } else if (loadVideo) {
+                    vbox.getChildren().addAll(userName, message, mediaView);
+                } else {
+                    vbox.getChildren().addAll(userName, message);
                 }
 
                 cell.setAlignment(Pos.CENTER_RIGHT);
                 cell.getChildren().addAll(vbox);
-
             }
             this.setGraphic(cell);
 
@@ -208,22 +219,71 @@ public class MessageListCell implements javafx.util.Callback<ListView<Message>, 
                 urlType = "picture";
             } else if (url.contains(".gif")) {
                 urlType = "gif";
-            } else {
+            }
+            else if (url.contains(".mp4")){
+                urlType = "video";
+            }
+            else {
                 urlType = "None";
             }
             return url;
         }
 
-        private void setImage(String url, WebEngine engine) {
+        private void setVideo(String url, MediaView mediaView) {
+            Media mediaUrl = new Media(url);
+            MediaPlayer mediaPlayer = new MediaPlayer(mediaUrl);
+            mediaPlayer.setAutoPlay(false);
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.setOnError(() -> System.out.println("Error : " + mediaPlayer.getError().toString()));
+        }
+
+        private void setMedia(String url, WebEngine engine, MediaView mediaView) {
             if (urlType.equals("picture")) {
                 engine.load(url);
                 loadImage = true;
+                engine.setJavaScriptEnabled(false);
             } else if (urlType.equals("gif")) {
                 engine.loadContent("<html><body><img src=\"" + url + "\" class=\"center\"></body></html>");
                 loadImage = true;
+                engine.setJavaScriptEnabled(false);
+            } else if (urlType.equals("video")) {
+                setVideo(url, mediaView);
+                loadVideo = true;
             }
-            engine.setJavaScriptEnabled(false);
-            engine.setUserStyleSheetLocation(Objects.requireNonNull(StageManager.class.getResource("styles/message/webView.css")).toExternalForm());
+            engine.setUserStyleSheetLocation(Objects.requireNonNull(getClass().getResource("/de/uniks/stp/styles/webView.css")).toExternalForm());
+        }
+
+
+        private void setVideoSize(String url, MediaView mediaView) {
+            try {
+                Parent parent = this.getParent();
+                while (parent.getParent() != null && (parent.getId() == null || parent.getId().equals("container"))) {
+                    parent = parent.getParent();
+                }
+                Bounds bounds = parent.getBoundsInLocal();
+                double maxX = bounds.getMaxX();
+                double maxY = bounds.getMaxY();
+                int height = 0;
+                int width = 0;
+                if (!urlType.equals("None")) {
+                    URL url_stream = new URL(url);
+                    BufferedImage image = ImageIO.read(url_stream.openStream());
+                    if (image != null) {
+                        height = image.getHeight();
+                        width = image.getWidth();
+                    }
+                }
+                if (height != 0 && width != 0 && (height < maxY - 50 || width < maxX - 50)) {
+                    mediaView.setFitHeight(height);
+                    mediaView.setFitWidth(width);
+                } else {
+                    mediaView.setFitHeight(maxY - 50);
+                    mediaView.setFitWidth(maxX - 50);
+                }
+                mediaView.autosize();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         private void setImageSize(String url, WebView webView) {
@@ -257,3 +317,4 @@ public class MessageListCell implements javafx.util.Callback<ListView<Message>, 
         }
     }
 }
+
