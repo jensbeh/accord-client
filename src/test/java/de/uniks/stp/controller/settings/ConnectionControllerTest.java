da871db20a38d3a4c9d619e5f35fc7fac269dfed
Maurice Golob <maurice.golob@gmail.com>
2021-07-29 23:50:13 +0200

    fix: headless fix

--- src/test/java/de/uniks/stp/controller/settings/ConnectionControllerTest.java
+++ src/test/java/de/uniks/stp/controller/settings/ConnectionControllerTest.java
@@ -87,7 +87,7 @@ public class ConnectionControllerTest extends ApplicationTest {
     @BeforeClass
     public static void setupHeadlessMode() {
         System.setProperty("testfx.robot", "glass");
-        System.setProperty("testfx.headless", "false");
+        System.setProperty("testfx.headless", "true");
         System.setProperty("headless.geometry", "1920x1080-32");
     }
 
