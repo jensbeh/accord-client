5959f59bd2920367ead4ff52111d32cafd16579a
Alex <alexbenz7@web.de>
2021-08-09 11:58:23 +0200

    feat: add WebsocketTest classes

---
+++ src/test/java/de/uniks/stp/net/websockets/privatesocket/PrivateSystemWebSocketClientTest.java
@@ -0,0 +1,135 @@
+package de.uniks.stp.net.websockets.privatesocket;
+
+import de.uniks.stp.StageManager;
+import de.uniks.stp.builder.ModelBuilder;
+import de.uniks.stp.controller.serverview.ServerMessageTest;
+import de.uniks.stp.net.RestClient;
+import de.uniks.stp.net.websocket.privatesocket.PrivateChatWebSocket;
+import de.uniks.stp.net.websocket.privatesocket.PrivateSystemWebSocketClient;
+import de.uniks.stp.net.websocket.serversocket.ServerChatWebSocket;
+import de.uniks.stp.net.websocket.serversocket.ServerSystemWebSocket;
+import javafx.stage.Stage;
+import kong.unirest.Callback;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class PrivateSystemWebSocketClientTest extends ApplicationTest {
+    private final String testServerName = "TestServer Team Bit Shift";
+    private final String testServerId = "5e2fbd8770dd077d03df505";
+    private final String testUserMainName = "Hendry Bracken";
+    private final String testUserMainPw = "stp2021pw";
+    @InjectMocks
+    StageManager mockApp = new StageManager();
+    private Stage stage;
+    private StageManager app;
+    @Mock
+    private PrivateSystemWebSocketClient privateSystemWebSocketClient;
+    @Mock
+    private PrivateChatWebSocket privateChatWebSocket;
+    @Mock
+    private ServerSystemWebSocket serverSystemWebSocket;
+    @Mock
+    private ServerChatWebSocket serverChatWebSocket;
+    @Mock
+    private RestClient restClient;
+    @Mock
+    private HttpResponse<JsonNode> response;
+    @Mock
+    private HttpResponse<JsonNode> response3;
+    @Captor
+    private ArgumentCaptor<Callback<JsonNode>> callbackCaptor;
+    @Captor
+    private ArgumentCaptor<Callback<JsonNode>> callbackCaptor3;
+
+    private ModelBuilder builder;
+
+    @BeforeClass
+    public static void setupHeadlessMode() {
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("headless.geometry", "1920x1080-32");
+    }
+
+    @BeforeAll
+    static void setup() {
+        MockitoAnnotations.openMocks(ServerMessageTest.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //start application
+        builder = new ModelBuilder();
+        builder.setUSER_CLIENT(privateSystemWebSocketClient);
+        builder.setPrivateChatWebSocketClient(privateChatWebSocket);
+        builder.setSERVER_USER(serverSystemWebSocket);
+        builder.setServerChatWebSocketClient(serverChatWebSocket);
+        this.stage = stage;
+        app = mockApp;
+        StageManager.setBuilder(builder);
+        StageManager.setRestClient(restClient);
+
+        builder.setLoadUserData(false);
+        mockApp.getBuilder().setSpotifyShow(false);
+        mockApp.getBuilder().setSpotifyToken(null);
+        mockApp.getBuilder().setSpotifyRefresh(null);
+
+        app.start(stage);
+        this.stage.centerOnScreen();
+    }
+
+    public void mockLogin() {
+        JSONObject jsonString = new JSONObject()
+                .put("status", "success")
+                .put("message", "")
+                .put("data", new JSONObject().put("userKey", testUserMainPw));
+        String jsonNode = new JsonNode(jsonString.toString()).toString();
+        when(response.getBody()).thenReturn(new JsonNode(jsonNode));
+        doAnswer((Answer<Void>) invocation -> {
+            Callback<JsonNode> callback = callbackCaptor.getValue();
+            callback.completed(response);
+            mockGetServers();
+            return null;
+        }).when(restClient).login(anyString(), anyString(), callbackCaptor.capture());
+    }
+
+    public void mockGetServers() {
+        JSONObject jsonString = new JSONObject()
+                .put("status", "success")
+                .put("message", "")
+                .put("data", new JSONArray().put(new JSONObject().put("id", "5e2fbd8770dd077d03df505").put("name", testServerName)));
+        String jsonNode = new JsonNode(jsonString.toString()).toString();
+        when(response3.getBody()).thenReturn(new JsonNode(jsonNode));
+        doAnswer((Answer<Void>) invocation -> {
+            Callback<JsonNode> callback = callbackCaptor3.getValue();
+            callback.completed(response3);
+            return null;
+        }).when(restClient).getServers(anyString(), callbackCaptor3.capture());
+    }
+
+    @Test
+    public void testPrivateSystemWebSocketTest() throws InterruptedException,IOException {
+        doCallRealMethod().when(privateSystemWebSocketClient).onOpen(any(),any()); //TODO zusätzlich onMessage
+        doCallRealMethod().when(privateSystemWebSocketClient).onClose(any(),any());
+        doCallRealMethod().when(privateSystemWebSocketClient).sendMessage(any());
+        doCallRealMethod().when(privateSystemWebSocketClient).onOpen(any(),any());
+        doCallRealMethod().when(privateSystemWebSocketClient).stop();
+        doCallRealMethod().when(privateSystemWebSocketClient).handleMessage(any()); //TODO updateServerUsers durch updateUserDescription
+//        mockLogin();
+    }
+}
---
+++ src/test/java/de/uniks/stp/net/websockets/serversocket/ServerSystemWebSocketTest.java
@@ -0,0 +1,134 @@
+package de.uniks.stp.net.websockets.serversocket;
+
+import de.uniks.stp.StageManager;
+import de.uniks.stp.builder.ModelBuilder;
+import de.uniks.stp.controller.serverview.ServerMessageTest;
+import de.uniks.stp.net.RestClient;
+import de.uniks.stp.net.websocket.privatesocket.PrivateChatWebSocket;
+import de.uniks.stp.net.websocket.privatesocket.PrivateSystemWebSocketClient;
+import de.uniks.stp.net.websocket.serversocket.ServerChatWebSocket;
+import de.uniks.stp.net.websocket.serversocket.ServerSystemWebSocket;
+import javafx.stage.Stage;
+import kong.unirest.Callback;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ServerSystemWebSocketTest extends ApplicationTest {
+    private final String testServerName = "TestServer Team Bit Shift";
+    private final String testServerId = "5e2fbd8770dd077d03df505";
+    private final String testUserMainName = "Hendry Bracken";
+    private final String testUserMainPw = "stp2021pw";
+    @InjectMocks
+    StageManager mockApp = new StageManager();
+    private Stage stage;
+    private StageManager app;
+    @Mock
+    private PrivateSystemWebSocketClient privateSystemWebSocketClient;
+    @Mock
+    private PrivateChatWebSocket privateChatWebSocket;
+    @Mock
+    private ServerSystemWebSocket serverSystemWebSocket;
+    @Mock
+    private ServerChatWebSocket serverChatWebSocket;
+    @Mock
+    private RestClient restClient;
+    @Mock
+    private HttpResponse<JsonNode> response;
+    @Mock
+    private HttpResponse<JsonNode> response3;
+    @Captor
+    private ArgumentCaptor<Callback<JsonNode>> callbackCaptor;
+    @Captor
+    private ArgumentCaptor<Callback<JsonNode>> callbackCaptor3;
+
+    private ModelBuilder builder;
+
+    @BeforeClass
+    public static void setupHeadlessMode() {
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("headless.geometry", "1920x1080-32");
+    }
+
+    @BeforeAll
+    static void setup() {
+        MockitoAnnotations.openMocks(ServerMessageTest.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //start application
+        builder = new ModelBuilder();
+        builder.setUSER_CLIENT(privateSystemWebSocketClient);
+        builder.setPrivateChatWebSocketClient(privateChatWebSocket);
+        builder.setSERVER_USER(serverSystemWebSocket);
+        builder.setServerChatWebSocketClient(serverChatWebSocket);
+        this.stage = stage;
+        app = mockApp;
+        StageManager.setBuilder(builder);
+        StageManager.setRestClient(restClient);
+
+        builder.setLoadUserData(false);
+        mockApp.getBuilder().setSpotifyShow(false);
+        mockApp.getBuilder().setSpotifyToken(null);
+        mockApp.getBuilder().setSpotifyRefresh(null);
+
+        app.start(stage);
+        this.stage.centerOnScreen();
+    }
+
+    public void mockLogin() {
+        JSONObject jsonString = new JSONObject()
+                .put("status", "success")
+                .put("message", "")
+                .put("data", new JSONObject().put("userKey", testUserMainPw));
+        String jsonNode = new JsonNode(jsonString.toString()).toString();
+        when(response.getBody()).thenReturn(new JsonNode(jsonNode));
+        doAnswer((Answer<Void>) invocation -> {
+            Callback<JsonNode> callback = callbackCaptor.getValue();
+            callback.completed(response);
+            mockGetServers();
+            return null;
+        }).when(restClient).login(anyString(), anyString(), callbackCaptor.capture());
+    }
+
+    public void mockGetServers() {
+        JSONObject jsonString = new JSONObject()
+                .put("status", "success")
+                .put("message", "")
+                .put("data", new JSONArray().put(new JSONObject().put("id", "5e2fbd8770dd077d03df505").put("name", testServerName)));
+        String jsonNode = new JsonNode(jsonString.toString()).toString();
+        when(response3.getBody()).thenReturn(new JsonNode(jsonNode));
+        doAnswer((Answer<Void>) invocation -> {
+            Callback<JsonNode> callback = callbackCaptor3.getValue();
+            callback.completed(response3);
+            return null;
+        }).when(restClient).getServers(anyString(), callbackCaptor3.capture());
+    }
+
+    @Test
+    public void testServerSystemWebSocketTest() throws InterruptedException, IOException {
+        doCallRealMethod().when(serverSystemWebSocket).onOpen(any(),any()); //TODO auch noch onMessage
+        doCallRealMethod().when(serverSystemWebSocket).onClose(any(),any());
+        doCallRealMethod().when(serverSystemWebSocket).stop();
+        doCallRealMethod().when(serverSystemWebSocket).handleMessage(any());//TODO userJoined userLeft und deleteServer(), deleteServer() (serverDeletedAlert, und saemtliche anderen methoden)
+//        mockLogin();
+
+    }
+}
